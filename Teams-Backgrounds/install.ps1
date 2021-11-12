e3470f22560202160c616601c404f9c41eb5436e
FlorianSLZ <38555854+FlorianSLZ@users.noreply.github.com>
2021-11-12 09:20:00 +0100

    Teams bg

---
+++ Teams-Backgrounds/install.ps1
@@ -0,0 +1,21 @@
+﻿$PackageName = "Teams-Backgrounds"
+
+$Path_4netIntune = "$Env:Programfiles\4net\EndpointManager"
+Start-Transcript -Path "$Path_4netIntune\Log\$PackageName-$env:USERNAME-install.log" -Force
+
+
+# Lokaler Ordner 
+$TeamsBG_Folder = "$env:APPDATA\Microsoft\Teams\Backgrounds\Uploads"
+ 
+# Sicherstelen, das der Ordner vorhanden ist
+New-Item -ItemType directory -Path $TeamsBG_Folder -Force
+ 
+# Hintergründe kopieren
+Copy-Item -path '.\bg' -Filter *.* -Destination $TeamsBG_Folder -Recurse
+
+New-Item -Path "$env:localAPPDATA\4net\EndpointManager\Validation\$PackageName" -ItemType "file" -Force
+
+Stop-Transcript
+
+
+
---
+++ Teams-Backgrounds/uninstall.ps1
@@ -0,0 +1,15 @@
+﻿$PackageName = "Teams-Backgrounds"
+
+$Path_4netIntune = "$Env:Programfiles\4net\EndpointManager"
+Start-Transcript -Path "$Path_4netIntune\Log\uninstall\$PackageName-$env:USERNAME-uninstall.log" -Force
+
+$TeamsBG_Folder = "$env:APPDATA\Microsoft\Teams\Backgrounds\Uploads"
+$TeamsBG_Files = Get-ChildItem -Path '.\bg' -Name
+
+# Verteilte Hintergründe löschen
+Get-ChildItem $TeamsBG_Folder | Where{$_.Name -in $TeamsBG_Files} | Remove-Item
+
+# Erkennungs File löschen
+Remove-Item -Path "$env:localAPPDATA\Intune\Validation\$PackageName" -Force
+
+Stop-Transcript
