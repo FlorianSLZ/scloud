9c67dff3ad65b96cf7c428544d02794dc04519cf
FlorianSLZ <38555854+FlorianSLZ@users.noreply.github.com>
2022-02-02 22:09:21 -0800

    update csv

--- DesktopShortcut/install.ps1
+++ DesktopShortcut/install.ps1
@@ -6,32 +6,36 @@ Start-Transcript -Path "$Path_4netIntune\Log\$PackageName-install.log" -Force
 
 # Paths
 $DesktopTMP = "$Path_4netIntune\Data\Desktop\$PackageName"
-$DesktopIcons = "$Path_4netIntune\Data\Desktop\icons"
+$DesktopIcons = "$Path_4netIntune\Data\icons\$PackageName"
 
 # Create Folders
 New-Item -Path $DesktopTMP -ItemType directory -force
 New-Item -Path $DesktopIcons -ItemType directory -force
 New-Item -Path "C:\Users\Public\Desktop" -ItemType directory -force
 
-# Remove old icons
+# Remove old shortcuts and icons
 $OLD_Items = Get-ChildItem -Path $DesktopTMP
 foreach($OLD_Item in $OLD_Items){
     Remove-Item "C:\Users\Public\Desktop\$($OLD_Item.Name)" -Force
 }
 Remove-Item "$DesktopTMP\*" -Force
+Remove-Item "$DesktopIcons\*" -Force
 
-# Icons from list
-$icons = import-csv link-list.csv
-foreach($icon in $icons){
-    $WshShell = New-Object -comObject WScript.Shell
-    $Shortcut = $WshShell.CreateShortcut("$DesktopTMP\$($icon.name).lnk")
-    $Shortcut.TargetPath = $icon.link
-    $Shortcut.Save()
-}
-
-# Copy New icons
+# Copy New shortcuts
 Copy-Item -Path ".\Desktop\*" -Destination $DesktopTMP -Recurse
 Copy-Item -Path ".\icons\*" -Destination $DesktopIcons -Recurse
+
+# shortcuts from list
+$shortcuts = Import-CSV "link-list.csv"
+foreach($shortcut in $shortcuts){
+    $WshShell = New-Object -comObject WScript.Shell
+    $Shortcut_file = $WshShell.CreateShortcut("$DesktopTMP\$($shortcut.name).lnk")
+    $Shortcut_file.TargetPath = $shortcut.link
+    $Shortcut_file.IconLocation = "$DesktopIcons\$($shortcut.icon)"
+    $Shortcut_file.Save()
+}
+
+# Copy icons to public Desktop
 Copy-Item -Path "$DesktopTMP\*" -Destination "C:\Users\Public\Desktop" -Recurse
 
 # Validation
