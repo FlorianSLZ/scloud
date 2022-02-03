c3a5641cd01d83922433853e828c6c1d60eec70f
FlorianSLZ <38555854+FlorianSLZ@users.noreply.github.com>
2022-02-03 07:08:24 +0100

    vlc added

---
+++ intune-win32-deployment/VLC/check.ps1
@@ -0,0 +1,10 @@
+﻿$ProgramName = "vlc"
+
+$localprograms = C:\ProgramData\chocolatey\choco.exe list --localonly
+if ($localprograms -like "*$ProgramName*"){
+    Write-Host "Found it!"
+}else{
+    Write-Host "Not Found!"
+    exit 1618 
+}
+
