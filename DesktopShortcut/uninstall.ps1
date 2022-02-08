d79c581a0ccfdbb2bd4602b00eaa4fea49235148
FlorianSLZ <38555854+FlorianSLZ@users.noreply.github.com>
2022-02-08 14:19:57 +0100

    wrong path

--- DesktopShortcut/uninstall.ps1
+++ DesktopShortcut/uninstall.ps1
@@ -1,17 +1,18 @@
-﻿$ProgramName = "eOpale"
+﻿$PackageName = "DesktopIcon_SLZ"
 
 $Path_4netIntune = "$Env:Programfiles\4net\EndpointManager"
 Start-Transcript -Path "$Path_4netIntune\Log\uninstall\$ProgramName-uninstall.log" -Force
 
+# remove old icons form package
 $DesktopTMP = "$Path_4netIntune\Data\Desktop\$ProgramName"
-New-Item -Path $DesktopTMP -ItemType directory -force
-
 $OLD_Items = Get-ChildItem -Path $DesktopTMP
 foreach($OLD_Item in $OLD_Items){
     Remove-Item "C:\Users\Public\Desktop\$($OLD_Item.Name)" -Force
 }
 Remove-Item "$DesktopTMP" -Force -Recurse
-Remove-Item -Path "C:\Admin\Intune\Validation\$PackageName" -Force
+
+# remove validation file wit version
+Remove-Item -Path "$Path_4netIntune\Validation\$PackageName" -Force
 
 Stop-Transcript
 
