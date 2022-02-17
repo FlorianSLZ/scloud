bb0b69b95e78ab24aa0c8c7ac8e7f16b01cb0715
FlorianSLZ <38555854+FlorianSLZ@users.noreply.github.com>
2022-02-17 07:32:52 +0100

    Update uninstall.ps1

--- Fonts/uninstall.ps1
+++ Fonts/uninstall.ps1
@@ -1,17 +1,26 @@
-﻿$WorkingPath = "C:\Admin\Intune\Fonts"
+﻿$PackageName = "Company-Fonts"
+
+$Path_4netIntune = "$Env:Programfiles\4net\EndpointManager"
+Start-Transcript -Path "$Path_4netIntune\Log\uninstall\$PackageName-install.log" -Force
+
+$WorkingPath = "$Path_4netIntune\Data\Fonts"
 New-Item -ItemType "directory" -Path $WorkingPath -Force
-Copy-Item -Path ".\Schriften\*" -Destination $WorkingPath -Recurse
+Copy-Item -Path ".\Fonts\*" -Destination $WorkingPath -Recurse
 
 $AllFonts = Get-ChildItem -Path "$WorkingPath\*.ttf"
 $AllFonts += Get-ChildItem -Path "$WorkingPath\*.otf"
 
 foreach($FontFile in $AllFonts){
     try{
-        Remove-Item -Path "$env:windir\Fonts\$($FontFile.Name)" -Force
+        Remove-Item -Path "$WorkingPath\$($FontFile.Name)" -Force
         Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" -Name $FontFile.Name -Force
     }catch{
-        Write-Error $_
+        Write-Host $_
     }
 }
 
-Remove-Item -Path $WorkingPath -Recurse -Force
+Remove-Item $WorkingPath -Force -Recurse
+
+Remove-Item -Path "$Path_4netIntune\Validation\$PackageName" -ItemType "file" -Force -Value $Version
+
+Stop-Transcript
