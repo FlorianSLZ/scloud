08ca65c9cde79b219ad8efe05dd0f64e0cee0e01
FlorianSLZ <38555854+FlorianSLZ@users.noreply.github.com>
2022-03-01 15:45:18 +0100

    update

--- Normal template/check.ps1
+++ Normal template/check.ps1
@@ -1,7 +1,10 @@
 $PackageName = "Normal-template"
-$ProgramVersion_target = '1'
+$Version = '1'
+
 $ProgramVersion_current = Get-Content -Path "$env:localAPPDATA\4net\EndpointManager\Validation\$PackageName"
 
-if($ProgramVersion_current -eq $ProgramVersion_target){
+if($ProgramVersion_current -eq $Version){
     Write-Host "Found it!"
 }
+
+
--- Normal template/install.ps1
+++ Normal template/install.ps1
@@ -1,12 +1,13 @@
 ﻿$PackageName = "Normal-template"
+$Version = "1"
 
 $Path_4netIntune = "$Env:Programfiles\4net\EndpointManager"
 Start-Transcript -Path "$Path_4netIntune\Log\$PackageName-$env:USERNAME-install.log" -Force
 
 try{
-New-Item -Path "$env:APPDATA\Microsoft\Templates" -ItemType "Directory" -Force
-Copy-Item 'Normal.dotm' -Destination "$env:APPDATA\Microsoft\Templates\" -Recurse -Force
-New-Item -Path "$env:localAPPDATA\4net\EndpointManager\Validation\$PackageName" -ItemType "file" -Force -Value 1
+    New-Item -Path "$env:APPDATA\Microsoft\Templates" -ItemType "Directory" -Force
+    Copy-Item 'Normal.dotm' -Destination "$env:APPDATA\Microsoft\Templates\" -Recurse -Force
+    New-Item -Path "$env:localAPPDATA\4net\EndpointManager\Validation\$PackageName" -ItemType "file" -Force -Value $Version
 }catch{
     Write-Host "_____________________________________________________________________"
     Write-Host "FEHLER"
