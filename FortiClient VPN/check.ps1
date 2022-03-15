3a5a8e8316c500fe888ba0b6ffdae1badd7e4c28
FlorianSLZ <38555854+FlorianSLZ@users.noreply.github.com>
2022-03-15 13:29:26 +0100

    Update check.ps1

--- FortiClient VPN/check.ps1
+++ FortiClient VPN/check.ps1
@@ -1,8 +1,9 @@
 $ProgramName = "FortiClientVPN"
+$ProfileName = "DEMO scloud" # Change to your Profilename!
 $ProgramPath = "C:\Program Files\Fortinet\FortiClient\FortiClient.exe"
 $ProgramVersion_target = '7.0.2.90'
 $ProgramVersion_current = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($ProgramPath).FileVersion
-$RegPath = 'HKLM:\SOFTWARE\Fortinet\FortiClient\Sslvpn\Tunnels\XYZ' # Hier den Namen anpassen!!
+$RegPath = "HKLM:\SOFTWARE\Fortinet\FortiClient\Sslvpn\Tunnels\$ProfileName" 
 $RegContent = Get-ItemProperty -Path $RegPath
 
 if(($ProgramVersion_current -eq $ProgramVersion_target) -and ($RegContent)){
