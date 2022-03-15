8e35f41af37d028998f0f3c9c96a0138abb99d70
FlorianSLZ <38555854+FlorianSLZ@users.noreply.github.com>
2022-03-15 14:58:25 +0100

    Update check.ps1

--- FortiClient VPN/check.ps1
+++ FortiClient VPN/check.ps1
@@ -1,7 +1,7 @@
 $ProgramName = "FortiClientVPN"
 $ProfileName = "DEMO scloud" # Change to your Profilename!
+$ProgramVersion_target = '7.0.2.90' # Set to version from MSI
 $ProgramPath = "C:\Program Files\Fortinet\FortiClient\FortiClient.exe"
-$ProgramVersion_target = '7.0.2.90'
 $ProgramVersion_current = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($ProgramPath).FileVersion
 $RegPath = "HKLM:\SOFTWARE\Fortinet\FortiClient\Sslvpn\Tunnels\$ProfileName" 
 $RegContent = Get-ItemProperty -Path $RegPath
