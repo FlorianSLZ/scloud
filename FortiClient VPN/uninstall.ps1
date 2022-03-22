7472650cc3ff3f257b47df6193132499af5ca014
FlorianSLZ <38555854+FlorianSLZ@users.noreply.github.com>
2022-03-22 08:29:41 +0100

    Update uninstall.ps1

--- FortiClient VPN/uninstall.ps1
+++ FortiClient VPN/uninstall.ps1
@@ -1,4 +1,6 @@
 ﻿$PackageName = "FortiClientVPN"
+
+$Path_4netIntune = "$Env:Programfiles\4net\EndpointManager"
 Start-Transcript -Path "$Path_4netIntune\Log\uninstall\$PackageName-uninstall.log" -Force
 
 Get-Package 'FortiClient VPN' | Uninstall-Package -Force
