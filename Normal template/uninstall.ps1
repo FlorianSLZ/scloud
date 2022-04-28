94bd0fa2c91efd41938092c5a95890403506defb
FlorianSLZ <38555854+FlorianSLZ@users.noreply.github.com>
2022-04-28 11:59:04 +0200

    Update uninstall.ps1

--- Normal template/uninstall.ps1
+++ Normal template/uninstall.ps1
@@ -1,8 +1,9 @@
-﻿$PackageName = "Normal-template"
+﻿$PackageName = "NormalEmail-template"
 
-Start-Transcript -Path "C:\Admin\Intune\Log\$PackageName-$env:USERNAME-uninstall.log" -Force
+$Path_4netIntune = "$Env:Programfiles\4net\EndpointManager"
+Start-Transcript -Path "$Path_4netIntune\Log\uninstall\$PackageName-$env:USERNAME-uninstall.log" -Force
 
-Remove-Item -Path "$env:APPDATA\Microsoft\Templates\Normal.dotm" -Force
-Remove-Item -Path "$env:localAPPDATA\Intune\Validation\$PackageName" -Force
+Remove-Item -Path "$env:APPDATA\Microsoft\Templates\NormalEmail.dotm" -Force
+Remove-Item -Path "$env:localAPPDATA\4net\EndpointManager\Validation\$PackageName" -Force
 
 Stop-Transcript
