d5a2d698e2c4caa741a39381f97298f313dce5b5
FlorianSLZ <38555854+FlorianSLZ@users.noreply.github.com>
2022-05-03 11:36:58 +0200

    correction Normal.dotm
    Changed error (NormalEmail.dotm to Normal.dotm

--- Normal template/uninstall.ps1
+++ Normal template/uninstall.ps1
@@ -1,9 +1,9 @@
-﻿$PackageName = "NormalEmail-template"
+﻿$PackageName = "Normal-template"
 
 $Path_4netIntune = "$Env:Programfiles\4net\EndpointManager"
 Start-Transcript -Path "$Path_4netIntune\Log\uninstall\$PackageName-$env:USERNAME-uninstall.log" -Force
 
-Remove-Item -Path "$env:APPDATA\Microsoft\Templates\NormalEmail.dotm" -Force
+Remove-Item -Path "$env:APPDATA\Microsoft\Templates\Normal.dotm" -Force
 Remove-Item -Path "$env:localAPPDATA\4net\EndpointManager\Validation\$PackageName" -Force
 
 Stop-Transcript
