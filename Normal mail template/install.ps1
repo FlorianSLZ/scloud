14ee83dc773d511c14daebbf83cef81d17fee8eb
FlorianSLZ <38555854+FlorianSLZ@users.noreply.github.com>
2022-06-07 20:53:09 +0200

    Update install.ps1

--- Normal mail template/install.ps1
+++ Normal mail template/install.ps1
@@ -1,9 +1,38 @@
 ﻿$PackageName = "NormalEmail-template"
 $Version = "1"
 
+# Font Settings Outlook
+$ComposeFontComplex = ""
+$ComposeFontSimple = ""
+$MarkCommentsWith = ""
+$ReplyFontComplex = ""
+$ReplyFontSimple = ""
+$TextFontComplex = ""
+$TextFontSimple = ""
+
+# Transcript for local log
 $Path_4netIntune = "$Env:Programfiles\4net\EndpointManager"
 Start-Transcript -Path "$Path_4netIntune\Log\$PackageName-$env:USERNAME-install.log" -Force
 $ErrorActionPreference = "Stop"
+
+#######################################################################################################################################
+#   Font Settings
+#######################################################################################################################################
+$Path = "HKCU:\Software\Policies\Microsoft\Office\16.0\Common\MailSettings" 
+if(!(Test-Path $Path)){New-Item -Path $Path -Force}
+
+if($ComposeFontComplex){Set-ItemProperty -Path $Path -Name "ComposeFontComplex" -Value $ComposeFontComplex -Type "Binary"}
+if($ComposeFontSimple){Set-ItemProperty -Path $Path -Name "ComposeFontSimple" -Value $ComposeFontSimple -Type "Binary"}
+if($MarkCommentsWith){Set-ItemProperty -Path $Path -Name "MarkCommentsWith" -Value $MarkCommentsWith -Type "Binary"}
+if($ReplyFontComplex){Set-ItemProperty -Path $Path -Name "ReplyFontComplex" -Value $ReplyFontComplex -Type "Binary"}
+if($ReplyFontSimple){Set-ItemProperty -Path $Path -Name "ReplyFontSimple" -Value $ReplyFontSimple -Type "Binary"}
+if($TextFontComplex){Set-ItemProperty -Path $Path -Name "TextFontComplex" -Value $TextFontComplex -Type "Binary"}
+if($TextFontSimple){Set-ItemProperty -Path $Path -Name "TextFontSimple" -Value $TextFontSimple -Type "Binary"}
+
+
+#######################################################################################################################################
+#   NormailEmail.dotm
+#######################################################################################################################################
 
 $NormalEmail_File = "$env:APPDATA\Microsoft\Templates\NormalEmail.dotm"
 
