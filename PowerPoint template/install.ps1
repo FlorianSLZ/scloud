32fcaceb1c5ad5322030d56617a30985095063fb
FlorianSLZ <38555854+FlorianSLZ@users.noreply.github.com>
2022-06-21 14:11:14 +0200

    template update

--- PowerPoint template/install.ps1
+++ PowerPoint template/install.ps1
@@ -8,15 +8,10 @@ try{
     # Create Templates folder
     New-Item -Path "$env:APPDATA\Microsoft\Templates" -ItemType "Directory" -Force
     # Copy template
-    Copy-Item -Path "Powerpoint_template.potx" -Destination "$env:APPDATA\Microsoft\Templates\blank.potx" -Recurse -Force
+    Copy-Item -Path "blank.potx" -Destination "$env:APPDATA\Microsoft\Templates\blank.potx" -Recurse -Force
 
     # Validation File
     New-Item -Path "$env:LOCALAPPDATA\4net\EndpointManager\Validation\$PackageName" -ItemType "file" -Force -Value $Version
-}catch{
-    Write-Host "_____________________________________________________________________"
-    Write-Host "ERROR"
-    Write-Host "$_"
-    Write-Host "_____________________________________________________________________"
-}
+}catch{$_}
 
 Stop-Transcript
