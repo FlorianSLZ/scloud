8b6cfa2110c0892f1d16114bc848736ecf95bd97
FlorianSLZ <38555854+FlorianSLZ@users.noreply.github.com>
2022-07-03 15:07:24 +0200

    add OneDrive desktop remove

--- Proactive Remediation/Proactive Remediation for Business/detection.ps1
+++ Proactive Remediation/Proactive Remediation for Business/detection.ps1
@@ -1,2 +1,4 @@
-$DesctopItems_copy = Get-ChildItem "C:\Users\*\Desktop\*- Copy*"
+$DesctopItems_copy = Get-ChildItem "C:\Users\*\Desktop\*- Copy*.lnk"
+$DesctopItems_copy += Get-ChildItem "C:\Users\*\OneDrive - *\Desktop\*- Copy*.lnk"
+
 If($DesctopItems_copy){exit 1}else{exit 0} # exit 1 = detectet, remediation needed
--- Proactive Remediation/Proactive Remediation for Business/remediation.ps1
+++ Proactive Remediation/Proactive Remediation for Business/remediation.ps1
@@ -1,1 +1,2 @@
-Remove-Item "C:\Users\*\Desktop\*- Copy*" -Force
+Remove-Item "C:\Users\*\Desktop\*- Copy*.lnk" -Force
+Remove-Item "C:\Users\*\OneDrive - *\Desktop\*- Copy*.lnk" -Force
