95ea90f3d510fc40f6bf50e211c2d84c2a35b55e
FlorianSLZ <38555854+FlorianSLZ@users.noreply.github.com>
2022-07-03 14:39:36 +0200

    directory change (cd)

--- Proactive Remediation/Proactive Remediation for Business/install.ps1
+++ Proactive Remediation/Proactive Remediation for Business/install.ps1
@@ -8,18 +8,22 @@ Start-Transcript -Path "$Path_4Log\Log\$PackageName-install.log" -Force
 #   Recurence Data
 ##########################################################################
 
-$Schedule_Frequency = "Daily"       # Once, Hourly, Daily, AtLogon
-$Schedule_RepeatInterval = "7"      # Number            (for Daily and Hourly)
+$Schedule_Frequency = "Hourly"       # Once, Hourly, Daily, AtLogon
+$Schedule_RepeatInterval = "1"      # Number            (for Daily and Hourly)
 $Schedule_StartDate = "2023-01-30"  # YYYY.MM.DD        (for Once)
 $Schedule_StartTime = "8am"         # ex 8am or 5pm     (for Once, Hourly, Daily)
 
 ##########################################################################
 
 try{
+    # local Path
+    $Path_PR = "$Path_4Log\Data\PR_$PackageName"
+
     # Check if Task exist with correct version
     $task_existing = Get-ScheduledTask -TaskName $PackageName -ErrorAction SilentlyContinue
     if($task_existing.Description -like "Version $Version*"){
-        $detection = .\detection.ps1
+        cd $Path_PR
+        .\detection.ps1
         if($LASTEXITCODE -ne 0){
             Write-Host "Detection positiv, remediation starts now"
             .\remediation.ps1
@@ -28,7 +32,6 @@ try{
             }
     }else{
         # path declaration / creation
-        $Path_PR = "$Path_4Log\Data\PR_$PackageName"
         New-Item -path $Path_PR -ItemType Directory -Force
         $Path_script = "$Path_PR\$PackageName.ps1"
         # get and save file content
@@ -36,6 +39,7 @@ try{
 
         # copy/safe detection- and remediation script
         Copy-Item detection.ps1 -Destination $Path_PR -Force
+        Copy-Item remediation.ps1 -Destination $Path_PR -Force
 
         # Register scheduled task to run at startup
         $schtaskDescription = "Version $Version"
