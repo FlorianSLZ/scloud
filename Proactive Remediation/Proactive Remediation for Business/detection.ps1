cc06740cb08da098e330db81758b40d7b52c06ec
FlorianSLZ <38555854+FlorianSLZ@users.noreply.github.com>
2022-07-03 11:33:33 +0200

    exit code

--- Proactive Remediation/Proactive Remediation for Business/detection.ps1
+++ Proactive Remediation/Proactive Remediation for Business/detection.ps1
@@ -1,2 +1,2 @@
 $DesctopItems_copy = Get-ChildItem "C:\Users\*\Desktop\*- Copy*"
-If($DesctopItems_copy){return 1} # exit 1 = detectet, remediation needed
+If($DesctopItems_copy){exit 1}else{exit 0} # exit 1 = detectet, remediation needed
