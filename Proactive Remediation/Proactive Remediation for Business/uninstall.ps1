fdfc0a0a22211871642e375c9707e5c8d7a4772c
FlorianSLZ <38555854+FlorianSLZ@users.noreply.github.com>
2022-07-05 09:04:44 +0200

    Update uninstall.ps1

--- Proactive Remediation/Proactive Remediation for Business/uninstall.ps1
+++ Proactive Remediation/Proactive Remediation for Business/uninstall.ps1
@@ -1,4 +1,4 @@
-﻿$PackageName = "FortiClientVPN"
+﻿$PackageName = "PR4Business"
 
 $Path_4netIntune = "$Env:Programfiles\4net\EndpointManager"
 Start-Transcript -Path "$Path_4netIntune\Log\uninstall\$PackageName-uninstall.log" -Force
