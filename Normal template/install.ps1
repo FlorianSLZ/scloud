6e9b5db6effc8b829d89b02f1feaf77cea2f4c01
FlorianSLZ <38555854+FlorianSLZ@users.noreply.github.com>
2022-07-18 08:11:36 +0200

    validation

--- Normal template/install.ps1
+++ Normal template/install.ps1
@@ -7,7 +7,7 @@ Start-Transcript -Path "$Path_4Log\Log\$PackageName-install.log" -Force
 try{
     New-Item -Path "$env:APPDATA\Microsoft\Templates" -ItemType "Directory" -Force
     Copy-Item 'Normal.dotm' -Destination "$env:APPDATA\Microsoft\Templates\" -Recurse -Force
-    New-Item -Path "$env:localAPPDATA\4net\EndpointManager\Validation\$PackageName" -ItemType "file" -Force -Value $Version
+    New-Item -Path "$Path_4Log\Validation\$PackageName" -ItemType "file" -Force -Value $Version
 }catch{
     Write-Host "_____________________________________________________________________"
     Write-Host "ERROR"
