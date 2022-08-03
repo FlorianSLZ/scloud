3b9da5ebe40950007007443b4d79c2b478cbd286
FlorianSLZ <38555854+FlorianSLZ@users.noreply.github.com>
2022-08-03 07:39:41 +0200

    win32

---
+++ MakeMeAdmin/MakeMeAdmin-Win32Package/check.ps1
@@ -0,0 +1,6 @@
+$ProgramName = "makemeadmin"
+
+$ChocoPrg_Existing = C:\ProgramData\chocolatey\choco.exe list --localonly
+    if ($ChocoPrg_Existing -like "*$ProgramName*"){
+    Write-Host "Found it!"
+}
---
+++ MakeMeAdmin/MakeMeAdmin-Win32Package/install.ps1
@@ -0,0 +1,13 @@
+﻿$ProgramName = "makemeadmin"
+
+$Path_4Log = "$Env:Programfiles\_MEM"
+Start-Transcript -Path "$Path_4Log\Log\$ProgramName-install.log" -Force
+
+$localprograms = C:\ProgramData\chocolatey\choco.exe list --localonly
+if ($localprograms -like "*$ProgramName*"){
+    C:\ProgramData\chocolatey\choco.exe upgrade $ProgramName -y
+}else{
+    C:\ProgramData\chocolatey\choco.exe install $ProgramName -y
+}
+
+Stop-Transcript
