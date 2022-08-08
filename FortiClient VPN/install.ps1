377a616b8904cade9a0f127ca9654971a10df922
FlorianSLZ <38555854+FlorianSLZ@users.noreply.github.com>
2022-08-08 13:23:22 +0200

    delay for import

--- FortiClient VPN/install.ps1
+++ FortiClient VPN/install.ps1
@@ -5,6 +5,7 @@ $Path_4netIntune = "$Env:Programfiles\4net\EndpointManager"
 Start-Transcript -Path "$Path_4netIntune\Log\$PackageName-install.log" -Force
 
 (Start-Process "msiexec.exe" -ArgumentList "/i FortiClientVPN.msi /passive /quiet INSTALLLEVEL=3 DESKTOPSHORTCUT=0 /NORESTART" -NoNewWindow -Wait -PassThru).ExitCode
+Start-Sleep 5
 Start-Process "C:\Program Files\Fortinet\FortiClient\FCConfig.exe" -ArgumentList "-m vpn -f FortiClientVPN.conf -o import -p $ConfigPW" -Wait
 
 Stop-Transcript
