23b52ce72e95ca1cf683c212dd716070ba92a4f5
FlorianSLZ <38555854+FlorianSLZ@users.noreply.github.com>
2022-08-09 13:00:54 +0200

    xltx ending

--- Excel template/install.ps1
+++ Excel template/install.ps1
@@ -8,8 +8,8 @@ try{
     # Create Excel\XLSTART folder
     New-Item -Path "$env:APPDATA\Microsoft\Excel\XLSTART" -ItemType "Directory" -Force
     # Copy templates (Book & Sheet)
-    Copy-Item -Path "Book.potx" -Destination "$env:APPDATA\Microsoft\Excel\XLSTART\Book.potx" -Recurse -Force
-    Copy-Item -Path "Sheet.potx" -Destination "$env:APPDATA\Microsoft\Excel\XLSTART\Sheet.potx" -Recurse -Force
+    Copy-Item -Path "Book.xltx" -Destination "$env:APPDATA\Microsoft\Excel\XLSTART\Book.xltx" -Recurse -Force
+    Copy-Item -Path "Sheet.xltx" -Destination "$env:APPDATA\Microsoft\Excel\XLSTART\Sheet.xltx" -Recurse -Force
 
     # Validation File
     New-Item -Path "$Path_4Log\Validation\$PackageName" -ItemType "file" -Force -Value $Version
