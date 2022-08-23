a23ed29ef8bf06d4029c4088a37f0c520dea1cea
FlorianSLZ <38555854+FlorianSLZ@users.noreply.github.com>
2022-08-23 19:54:46 +0200

    Update install.ps1

--- chocolatey/chocolatey/install.ps1
+++ chocolatey/chocolatey/install.ps1
@@ -4,18 +4,11 @@ Start-Transcript -Path "$Path_local\Log\$ProgramName-install.log" -Force
 
 try{
     if(!(test-path "C:\ProgramData\chocolatey\choco.exe")){
-        Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+        Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
     }
-
     C:\ProgramData\chocolatey\choco.exe list -lo
-
     choco feature enable -n=useRememberedArgumentsForUpgrades
-    
     exit 0
-}catch{
-    exit 1618
-}
-
+}catch{exit 1618}
 
 Stop-Transcript
-
