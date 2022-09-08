40e02f59eb92561b4f7419a972aa621157dd1c1d
FlorianSLZ <38555854+FlorianSLZ@users.noreply.github.com>
2022-09-08 15:56:20 +0200

    PAR winget updates

---
+++ Proactive Remediation/winget app update/detection-winget-upgrade.ps1
@@ -0,0 +1,23 @@
+Param(
+    [parameter(Mandatory=$false)]
+    [String[]]
+    $app_2upgrade = "WINGETPROGRAMID",
+
+	[parameter(Mandatory=$false)]
+    [String[]]
+	$version
+)
+
+# resolve and navigate to winget
+$Path_WingetAll = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
+if($Path_WingetAll){$Path_Winget = $Path_WingetAll[-1].Path}
+cd $Path_Winget
+
+if ($(.\winget upgrade) -like "* $app_2upgrade *") {
+	Write-Host "Upgrade aviable for: $app_2upgrade"
+	exit 1 # upgrade aviable, remediation needed
+}
+else {
+		Write-Host "No Upgrade aviable"
+		exit 0 # no upgared, no action needed
+}
---
+++ Proactive Remediation/winget app update/remediation-winget-upgrade.ps1
@@ -0,0 +1,24 @@
+Param(
+    [parameter(Mandatory=$false)]
+    [String[]]
+    $app_2upgrade = "WINGETPROGRAMID",
+
+	[parameter(Mandatory=$false)]
+    [String[]]
+	$version
+)
+
+try{
+    # resolve and navigate to winget
+    $Path_WingetAll = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
+    if($Path_WingetAll){$Path_Winget = $Path_WingetAll[-1].Path}
+    cd $Path_Winget
+
+    # upgrade command
+    .\winget.exe upgrade --exact $app_2upgrade --silent --force --accept-package-agreements --accept-source-agreements
+    exit 0
+
+}catch{
+    Write-Error "Error while installing upgarde for: $app_2upgrade"
+    exit 1
+}
