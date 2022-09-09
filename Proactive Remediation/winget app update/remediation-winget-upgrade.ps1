01bbfda1ac3bbb2a62197e78f018cebdc146d264
FlorianSLZ <38555854+FlorianSLZ@users.noreply.github.com>
2022-09-09 17:27:51 +0200

    Update remediation-winget-upgrade.ps1

--- Proactive Remediation/winget app update/remediation-winget-upgrade.ps1
+++ Proactive Remediation/winget app update/remediation-winget-upgrade.ps1
@@ -1,21 +1,12 @@
-Param(
-    [parameter(Mandatory=$false)]
-    [String[]]
-    $app_2upgrade = "WINGETPROGRAMID",
-
-	[parameter(Mandatory=$false)]
-    [String[]]
-	$version = "auto"
-)
+$app_2upgrade = "WINGETPROGRAMID"
+$version = "auto"
 
 try{
-    # resolve and navigate to winget
-    $Path_WingetAll = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
-    if($Path_WingetAll){$Path_Winget = $Path_WingetAll[-1].Path}
-    cd $Path_Winget
+    # resolve and navigate to winget.exe
+    $Winget = Get-ChildItem -Path (Join-Path -Path (Join-Path -Path $env:ProgramFiles -ChildPath "WindowsApps") -ChildPath "Microsoft.DesktopAppInstaller*_x64*\winget.exe")
 
     # upgrade command
-    .\winget.exe upgrade --exact $app_2upgrade --silent --force --accept-package-agreements --accept-source-agreements
+    &$winget upgrade --exact $app_2upgrade --silent --force --accept-package-agreements --accept-source-agreements
     exit 0
 
 }catch{
