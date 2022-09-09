eba1183501dcf5b71feb13f6988b27d1be809c01
FlorianSLZ <38555854+FlorianSLZ@users.noreply.github.com>
2022-09-09 17:27:19 +0200

    Update detection-winget-upgrade.ps1

--- Proactive Remediation/winget app update/detection-winget-upgrade.ps1
+++ Proactive Remediation/winget app update/detection-winget-upgrade.ps1
@@ -1,19 +1,10 @@
-Param(
-    [parameter(Mandatory=$false)]
-    [String[]]
-    $app_2upgrade = "WINGETPROGRAMID",
+$app_2upgrade = "WINGETPROGRAMID"
+$version = "auto"
 
-	[parameter(Mandatory=$false)]
-    [String[]]
-	$version = "auto"
-)
+# resolve and navigate to winget.exe
+$Winget = Get-ChildItem -Path (Join-Path -Path (Join-Path -Path $env:ProgramFiles -ChildPath "WindowsApps") -ChildPath "Microsoft.DesktopAppInstaller*_x64*\winget.exe")
 
-# resolve and navigate to winget
-$Path_WingetAll = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
-if($Path_WingetAll){$Path_Winget = $Path_WingetAll[-1].Path}
-cd $Path_Winget
-
-if ($(.\winget upgrade) -like "* $app_2upgrade *") {
+if ($(&$winget upgrade) -like "* $app_2upgrade *") {
 	Write-Host "Upgrade aviable for: $app_2upgrade"
 	exit 1 # upgrade aviable, remediation needed
 }
