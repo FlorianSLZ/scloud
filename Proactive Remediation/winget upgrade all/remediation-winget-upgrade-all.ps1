425d6d45242c3ca0903a3c3b68222cc366ee663b
Florian Salzmann <38555854+FlorianSLZ@users.noreply.github.com>
2022-09-13 10:48:56 +0200

    Update remediation-winget-upgrade-all.ps1

--- Proactive Remediation/winget upgrade all/remediation-winget-upgrade-all.ps1
+++ Proactive Remediation/winget upgrade all/remediation-winget-upgrade-all.ps1
@@ -2,7 +2,7 @@ try{
     $Winget = Get-ChildItem -Path (Join-Path -Path (Join-Path -Path $env:ProgramFiles -ChildPath "WindowsApps") -ChildPath "Microsoft.DesktopAppInstaller*_x64*\winget.exe")
 
     # upgrade command for ALL
-    &$Winget upgrade --query "" --all --silent --accept-package-agreements --accept-source-agreements --all
+    &$Winget upgrade --all #--query "" --silent --accept-package-agreements --accept-source-agreements
 
     exit 0
 
