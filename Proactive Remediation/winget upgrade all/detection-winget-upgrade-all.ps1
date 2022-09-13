29d0b6fe72031ca39a944e6ae3ce890d8d739295
Florian Salzmann <38555854+FlorianSLZ@users.noreply.github.com>
2022-09-13 10:30:20 +0200

    Update detection-winget-upgrade-all.ps1

--- Proactive Remediation/winget upgrade all/detection-winget-upgrade-all.ps1
+++ Proactive Remediation/winget upgrade all/detection-winget-upgrade-all.ps1
@@ -1,10 +1,10 @@
 $Winget = Get-ChildItem -Path (Join-Path -Path (Join-Path -Path $env:ProgramFiles -ChildPath "WindowsApps") -ChildPath "Microsoft.DesktopAppInstaller*_x64*\winget.exe")
 
 if ($(&$winget upgrade) -gt 3) {
-	Write-Host "Upgrade(s) aviable."
-	exit 1 # upgrade aviable, remediation needed
+	Write-Host "Upgrade(s) available."
+	exit 1 # upgrade available, remediation needed
 }
 else {
-		Write-Host "No Upgrade aviable"
+		Write-Host "No Upgrade available"
 		exit 0 # no upgared, no action needed
 }
