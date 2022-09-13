4ede31bef48b7021bfc487335c67777507f6fe45
Florian Salzmann <38555854+FlorianSLZ@users.noreply.github.com>
2022-09-13 10:31:31 +0200

    typo

--- Proactive Remediation/winget app update/detection-winget-upgrade.ps1
+++ Proactive Remediation/winget app update/detection-winget-upgrade.ps1
@@ -3,10 +3,10 @@ $app_2upgrade = "WINGETPROGRAMID"
 $Winget = Get-ChildItem -Path (Join-Path -Path (Join-Path -Path $env:ProgramFiles -ChildPath "WindowsApps") -ChildPath "Microsoft.DesktopAppInstaller*_x64*\winget.exe")
 
 if ($(&$winget upgrade) -like "* $app_2upgrade *") {
-	Write-Host "Upgrade aviable for: $app_2upgrade"
-	exit 1 # upgrade aviable, remediation needed
+	Write-Host "Upgrade available for: $app_2upgrade"
+	exit 1 # upgrade available, remediation needed
 }
 else {
-		Write-Host "No Upgrade aviable"
+		Write-Host "No Upgrade available"
 		exit 0 # no upgared, no action needed
 }
