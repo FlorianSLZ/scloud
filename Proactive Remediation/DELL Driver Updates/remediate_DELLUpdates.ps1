f457abce4cb572407d7db3c74c62ab2ad8c3043d
Florian Salzmann <38555854+FlorianSLZ@users.noreply.github.com>
2022-11-22 19:32:39 +0100

    Update remediate_DELLUpdates.ps1

--- Proactive Remediation/DELL Driver Updates/remediate_DELLUpdates.ps1
+++ Proactive Remediation/DELL Driver Updates/remediate_DELLUpdates.ps1
@@ -4,9 +4,10 @@ $DCU_exe = "$DCU_folder\dcu-cli.exe"
 $DCU_category = "firmware,driver"  # bios,firmware,driver,application,others
 
 try{
-    Start-Process $DCU_exe -ArgumentList "/applyUpdates -silent -reboot=disable -updateType=$DCU_category -outputlog=$DCU_report" -Wait
+    Start-Process $DCU_exe -ArgumentList "/applyUpdates -silent -reboot=disable -updateType=$DCU_category -outputlog=$DCU_report /configure -silent -autoSuspendBitLocker=enable -userConsent=disable" -Wait
     Write-Output "Installation completed"
 }catch{
     Write-Error $_.Exception
 }
 
+# Usermanual: https://www.dell.com/support/manuals/de-ch/command-update/dellcommandupdate_rg/dell-command-%7C-update-cli-commands?guid=guid-92619086-5f7c-4a05-bce2-0d560c15e8ed&lang=en-us
