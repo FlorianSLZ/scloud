2622965f119329e8d17ff25452770ff062a41f77
FlorianSLZ <38555854+FlorianSLZ@users.noreply.github.com>
2023-02-27 12:43:14 +0100

    log path alligment

--- PowerPoint template/uninstall.ps1
+++ PowerPoint template/uninstall.ps1
@@ -1,9 +1,9 @@
 ﻿$PackageName = "PowerPoint-template"
 
-$Path_4Log = "$ENV:LOCALAPPDATA\_MEM"
-Start-Transcript -Path "$Path_4Log\Log\$PackageName-uninstall.log" -Force
+$Path_local = "$ENV:LOCALAPPDATA\_MEM"
+Start-Transcript -Path "$Path_local\Log\$PackageName-uninstall.log" -Force
 
 Remove-Item -Path "$env:APPDATA\Microsoft\Templates\blank.potx" -Force
-Remove-Item -Path "$Path_4Log\Validation\$PackageName" -Force
+Remove-Item -Path "$Path_local\Validation\$PackageName" -Force
 
 Stop-Transcript
