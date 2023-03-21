8017d7043a14b5704e4ac7871e01b459c1bab6b5
FlorianSLZ <38555854+FlorianSLZ@users.noreply.github.com>
2023-03-21 08:14:41 +0100

    inline comments

--- Proactive Remediation/old Profile Monitor and Cleanup/remediation.ps1
+++ Proactive Remediation/old Profile Monitor and Cleanup/remediation.ps1
@@ -2,10 +2,13 @@ $Profile_age = 35 # max profile age in days
 
 Try {
 
-    # Get all User profiles older than x days
+    # Get all User profile folders older than X days
     $LastAccessedFolder = Get-ChildItem "C:\Users" |  Where-Object {$_ -notlike "*Windows*" -and $_ -notlike "*default*" -and $_ -notlike "*Public*" -and $_ -notlike "*Admin*"} | Where LastWriteTime -lt (Get-Date).AddDays(-$Profile_age)
 
+    # Filter the list of folders to only include those that are not associated with local user accounts
     $Profiles_notLocal = $LastAccessedFolder | Where-Object { $_.Name -notin $(Get-LocalUser).Name }
+
+    # Retrieve a list of user profiles and filter to only include the old ones
     $Profiles_2remove = Get-CimInstance -Class Win32_UserProfile | Where-Object { $_.LocalPath -in $($Profiles_notLocal.FullName) }
 
     if($Profiles_2remove){
