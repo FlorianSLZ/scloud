092acf3de6cea6b247722f2862279d4a217dc137
FlorianSLZ <38555854+FlorianSLZ@users.noreply.github.com>
2023-05-22 07:13:43 +0200

    Update MgId_addPermissions.ps1

--- Automations/AZ Automation with Managed Identity/MgId_addPermissions.ps1
+++ Automations/AZ Automation with Managed Identity/MgId_addPermissions.ps1
@@ -1,28 +1,22 @@
 # Install Microft Graph Module
 Install-Module Microsoft.Graph -Scope CurrentUser
 
-
 # Connect to Microsoft Graph
 Connect-MgGraph -Scopes "Application.Read.All","AppRoleAssignment.ReadWrite.All,RoleManagement.ReadWrite.Directory"
 
 # Select beta profile
 Select-MgProfile Beta
 
-
-# Change this to your Managed Identity app name:
-$managedIdentityName = "scloud"
-$managedIdentityId = (Get-MgServicePrincipal -Filter "displayName eq $managedIdentityName").id
-
+# You will be prompted for the Name of you Managed Identity
+$MdId_Name = Read-Host "Name of your Managed Identity"
+$MdId_ID = (Get-MgServicePrincipal -Filter "displayName eq '$MdId_Name'").id
 
 # Adding Microsoft Graph permissions
 $graphApp = Get-MgServicePrincipal -Filter "AppId eq '00000003-0000-0000-c000-000000000000'"
 
 # Add the required Graph scopes
 $graphScopes = @(
-  'UserAuthenticationMethod.Read.All',
-  'Group.ReadWrite.All',
-  'Directory.Read.All',
-  'User.ReadWrite.All'
+  "DeviceManagementManagedDevices.Read.All"
 )
 ForEach($scope in $graphScopes){
   $appRole = $graphApp.AppRoles | Where-Object {$_.Value -eq $scope}
@@ -30,10 +24,10 @@ ForEach($scope in $graphScopes){
   if ($null -eq $appRole) { Write-Warning "Unable to find App Role for scope $scope"; continue; }
 
   # Check if permissions isn't already assigned
-  $assignedAppRole = Get-MgServicePrincipalAppRoleAssignment -ServicePrincipalId $managedIdentityId | Where-Object { $_.AppRoleId -eq $appRole.Id -and $_.ResourceDisplayName -eq "Microsoft Graph" }
+  $assignedAppRole = Get-MgServicePrincipalAppRoleAssignment -ServicePrincipalId $MdId_ID | Where-Object { $_.AppRoleId -eq $appRole.Id -and $_.ResourceDisplayName -eq "Microsoft Graph" }
 
   if ($null -eq $assignedAppRole) {
-    New-MgServicePrincipalAppRoleAssignment -PrincipalId $managedIdentityId -ServicePrincipalId $managedIdentityId -ResourceId $graphApp.Id -AppRoleId $appRole.Id
+    New-MgServicePrincipalAppRoleAssignment -PrincipalId $MdId_ID -ServicePrincipalId $MdId_ID -ResourceId $graphApp.Id -AppRoleId $appRole.Id
   }else{
     write-host "Scope $scope already assigned"
   }
