5950c272b0cab763a7f3b6b5b7caf7d14dfcc5be
FlorianSLZ <38555854+FlorianSLZ@users.noreply.github.com>
2023-05-22 07:14:04 +0200

    Managed Identity Login

--- Automations/AZ Automation with Managed Identity/MgId_getOldDevices.ps1
+++ Automations/AZ Automation with Managed Identity/MgId_getOldDevices.ps1
@@ -1,10 +1,13 @@
-# Conenction with Managed Identity
-Connect-MgGraph -Identity
-
-Connect-MgGraph -Scopes "DeviceManagementManagedDevices.Read.All"
+# Connect to Microsoft Graph within Azure Automation (Microsoft Graph PowerShell v1)
+Connect-AzAccount -Identity
+$token = Get-AzAccessToken -ResourceUrl "https://graph.microsoft.com"
+Connect-MgGraph -AccessToken $token.Token
 
 # Define device age to include
 $inactiveDays = "180"
+
+# YOUR Webhook URL
+$WebHookURL = "https://xxxx.webhook.office.com/someID..."
 
 # Construct the Graph API request URI
 $graphUri = "https://graph.microsoft.com/beta/deviceManagement/managedDevices"
@@ -33,29 +36,28 @@ foreach ($device in $response.value) {
 $report
 
 
-###############################################################################
+if($report){
 
-# YOUR Webhook URL
-$WebHookURL = "https://xxxx.webhook.office.com/someID..."
-
-
-# Message JSON 
-$Message_Json = [PSCustomObject][Ordered]@{
-    "@type" = "MessageCard"
-    "@context" = "<http://schema.org/extensions>"
-    "summary" = "You have $($report.count) Inactive Devices which haven't have contatc in the last $inactiveDays"
-    "themeColor" = '0078D7'
-	"title" = "Inactive Devices ($($report.count))"
-    "text" = "<h1>Inactive Devices for $inactiveDays+ days</h1>
-    <pre>$($report | Format-Table DeviceName, LastSyncDateTime | Out-String)</pre>"
-} | ConvertTo-Json
+    # Message JSON 
+    $Message_Json = [PSCustomObject][Ordered]@{
+        "@type" = "MessageCard"
+        "@context" = "<http://schema.org/extensions>"
+        "summary" = "You have $($report.count) Inactive Devices which haven't have contatc in the last $inactiveDays"
+        "themeColor" = '0078D7'
+        "title" = "Inactive Devices ($($report.count))"
+        "text" = "<h1>Inactive Devices for $inactiveDays+ days</h1>
+        <pre>$($report | Format-Table DeviceName, LastSyncDateTime | Out-String)</pre>"
+    } | ConvertTo-Json
 
 
-$parameters = @{
-	"URI" = $WebHookURL
-	"Method" = 'POST'
-	"Body" = $Message_Json
-	"ContentType" = 'application/json'
+    $parameters = @{
+        "URI" = $WebHookURL
+        "Method" = 'POST'
+        "Body" = $Message_Json
+        "ContentType" = 'application/json'
+    }
+
+    Invoke-RestMethod @parameters
 }
 
-Invoke-RestMethod @parameters
+
