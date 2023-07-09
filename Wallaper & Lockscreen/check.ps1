7e77f186b7613ba1b77deb262640d96811a76670
Florian Salzmann <38555854+FlorianSLZ@users.noreply.github.com>
2023-07-09 18:43:01 +0200

    duplicate

--- Wallaper & Lockscreen/check.ps1
+++
@@ -1,8 +0,0 @@
-$PackageName = "Wallpaper"
-$Version = 1
-
-$ProgramVersion_current = Get-Content -Path "$Env:Programfiles\4net\EndpointManager\Validation\$PackageName"
-
-if($ProgramVersion_current -eq $Version){
-    Write-Host "Found it!"
-}
