2e201739758b80c3ce30b3e989ecec7c7a37c2af
Florian Salzmann <38555854+FlorianSLZ@users.noreply.github.com>
2023-07-09 19:08:07 +0200

    Update check.ps1

--- DesktopShortcut/check.ps1
+++ DesktopShortcut/check.ps1
@@ -1,6 +1,7 @@
 $PackageName = "DesktopIcon_SLZ"
 $Version = "1"
-$ProgramVersion_current = Get-Content -Path "$Env:Programfiles\4net\EndpointManager\Validation\$PackageName"
+$Path_local = "$Env:Programfiles\MEM"
+$ProgramVersion_current = Get-Content -Path "$Path_local\Validation\$PackageName"
 
 if($ProgramVersion_current -eq $Version){
     Write-Host "Found it!"
