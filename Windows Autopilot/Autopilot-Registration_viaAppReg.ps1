86f0a7d31e11b78017d5911ea162d0936042f2d4
Florian Salzmann <38555854+FlorianSLZ@users.noreply.github.com>
2023-08-30 17:07:04 +0200

    Create Autopilot-Registration_viaAppReg.ps1

---
+++ Windows Autopilot/Autopilot-Registration_viaAppReg.ps1
@@ -0,0 +1,17 @@
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Force
+Write-Host "(i) We will now install the some required components`n" -ForegroundColor Cyan
+Install-PackageProvider -Name NuGet -Force | Out-Null
+Install-Script -Name Get-WindowsAutoPilotInfo -Force | Out-Null
+Install-Module -Name WindowsAutopilotIntune -Force | Out-Null
+
+$GroupTag = Read-Host "To upload with a Group Tag, enter the Group Tag now. Otherwise, press Enter to continue without a Group Tag."
+
+$AutopilotParams = @{
+    Online = $true
+    TenantId = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+    AppId = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+    AppSecret = "xxxx~xxxxxxxxxxxxxx~xxxxxxxxxxxxxxxxxxxxx"
+    GroupTag = "$GroupTag"
+}
+
+Get-WindowsAutoPilotInfo @AutopilotParams
