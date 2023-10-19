8cd48b5d92e94c639a78efd1f5917314e47dcb31
Florian Salzmann <38555854+FlorianSLZ@users.noreply.github.com>
2023-10-19 15:49:10 +0200

    Transcript

--- Registry with Intune/Set-RegistryKey.ps1
+++ Registry with Intune/Set-RegistryKey.ps1
@@ -1,3 +1,6 @@
+$PackageName = "Registry-XY"
+Start-Transcript -Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\$PackageName-script.log" -Force
+
 $Path = "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power"
 $Key = "HiberbootEnabled"
 $KeyFormat = "dword"
@@ -6,3 +9,5 @@ $Value = "1"
 if(!(Test-Path $Path)){New-Item -Path $Path -Force}
 if(!$Key){Set-Item -Path $Path -Value $Value
 }else{Set-ItemProperty -Path $Path -Name $Key -Value $Value -Type $KeyFormat}
+
+Stop-Transcript
