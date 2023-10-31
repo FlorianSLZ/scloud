72cb672e148c5008192ac54cb17aebb8fd56fec1
Florian Salzmann <38555854+FlorianSLZ@users.noreply.github.com>
2023-10-31 07:55:40 +0100

    preparation for new teams

---
+++ Teams/Teams-Backgrounds/install.ps1
@@ -0,0 +1,34 @@
+﻿$PackageName = "Teams-Backgrounds"
+$Version = "1"
+
+Start-Transcript -Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\$PackageName-$env:USERNAME-install.log" -Force
+$ErrorActionPreference = "Stop"
+
+try{
+    # Local Folder 
+    $TeamsBG_Folder = "$env:LOCALAPPDATA\Packages\MSTeams_8wekyb3d8bbwe\LocalCache\Microsoft\MSTeams\Backgrounds\Uploads"
+    
+    # Sicherstelen, das der Ordner vorhanden ist
+    New-Item -ItemType directory -Path $TeamsBG_Folder -Force
+    
+    # Hintergründe kopieren
+    $vBackgrounds = Get-ChildItem -Path '.\bg' 
+    foreach($vBackground in $vBackgrounds){
+        $vBackground_guid = [guid]::NewGuid().Guid
+        Copy-Item -path $vBackground.FullName -Destination "$TeamsBG_Folder\$vBackground_guid$($vBackground.Extension)" -Force -WhatIf
+
+    }
+
+    # Validation Key
+    New-Item "HKCU:\SOFTWARE\scloud\Packages\$PackageName" -Force | New-ItemProperty -Name Version -Value $Version -Force
+
+
+    
+}catch{
+    Write-Host "_____________________________________________________________________"
+    Write-Host "ERROR"
+    Write-Host "$_"
+    Write-Host "_____________________________________________________________________"
+}
+
+Stop-Transcript
---
+++ Teams/Teams-Backgrounds/uninstall.ps1
@@ -0,0 +1,14 @@
+﻿$PackageName = "Teams-Backgrounds"
+
+Start-Transcript -Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\$PackageName-$env:USERNAME-uninstall.log" -Force
+
+$TeamsBG_Folder = "$env:APPDATA\Microsoft\Teams\Backgrounds\Uploads"
+$TeamsBG_Files = Get-ChildItem -Path '.\bg' -Name
+
+# delete deployed wallpapers
+Get-ChildItem $TeamsBG_Folder | Where-Object{$_.Name -in $TeamsBG_Files} | Remove-Item
+
+# delete detection
+Remove-Item "HKCU:\SOFTWARE\scloud\Packages\$PackageName"
+
+Stop-Transcript
