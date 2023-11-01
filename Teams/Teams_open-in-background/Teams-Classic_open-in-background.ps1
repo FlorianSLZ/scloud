47bc8622ae0120f02ccb61ef262740d416270cca
Florian Salzmann <38555854+FlorianSLZ@users.noreply.github.com>
2023-11-01 15:49:04 +0100

    moved to new location

---
+++ Teams/Teams_open-in-background/Teams-Classic_open-in-background.ps1
@@ -0,0 +1,49 @@
+$PackageName = "Teams-Classic_open-in-background"
+$Version = "1"
+
+$Path_local = "$env:LOCALAPPDATA\MEM"
+Start-Transcript -Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\$PackageName-install.log" -Force
+
+try{
+    # registry key
+    $KeyPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnce"
+    
+    # Check if Property exist with correct version
+    $PropertyName = "$PackageName - V$Version"
+    $Property_existing = Get-ItemProperty $KeyPath -Name $PropertyName -ErrorAction SilentlyContinue
+    if($Property_existing){
+
+        try{
+            ############################################################################################
+            #   CODE TO RUN ONCE
+
+            # End acitve Teams process
+            if(Get-Process Teams -ErrorAction SilentlyContinue){Get-Process Teams | Stop-Process -Force}
+            # Replace/Set "openAsHidden" option to true
+            (Get-Content $ENV:APPDATA\Microsoft\Teams\desktop-config.json -ErrorAction Stop).replace('"openAsHidden":false', '"openAsHidden":true') | Set-Content $ENV:APPDATA\Microsoft\Teams\desktop-config.json
+            # Start Teams in background
+            Start-Process -File $env:LOCALAPPDATA\Microsoft\Teams\Update.exe -ArgumentList '--processStart "Teams.exe" --process-start-args "--system-initiated"'
+
+            #   END CODE TO RUN ONCE
+            ############################################################################################
+
+        # Delete Script
+        Remove-Item -Path $MyInvocation.MyCommand.Source 
+        }catch{$_}
+
+    }else{
+        # script path
+        $script_path = "$Path_local\Data\$PackageName.ps1"
+        New-Item -ItemType Directory -Force -Path "$Path_local\Data" | Out-Null
+        # get and save file content
+        Get-Content -Path $($PSCommandPath) | Out-File -FilePath $script_path -Force
+
+        # Create Property with script to execute
+        Set-ItemProperty $KeyPath -Name $PropertyName -Value "C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -File `"$script_path`""
+    }
+}catch{
+    Write-Error $_
+}
+
+Stop-Transcript
+
