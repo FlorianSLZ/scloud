f46a78a4416f2d51336ad7dca1f7922fd2c55187
Florian Salzmann <38555854+FlorianSLZ@users.noreply.github.com>
2023-12-03 10:30:42 +0100

    choco update

--- chocolatey/chocolatey/install.ps1
+++ chocolatey/chocolatey/install.ps1
@@ -1,29 +1,35 @@
-﻿$PackageName = "chocolatey"
-
-Start-Transcript -Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\$PackageName-install.log" -Force
-
+﻿$AppInfo = Get-Content -Raw -Path "AppInfo.json" | ConvertFrom-Json
+Start-Transcript -Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\$($AppInfo.Name)-install.log" -Force
 
 try{
 
-    # Prüft choco Installation
-    if($(Test-Path "C:\ProgramData\chocolatey\choco.exe")){
-        # Upgrade chocolatey
-        C:\ProgramData\chocolatey\choco.exe upgrade chocolatey
-    }else{
-        # Install chocolatey
-        Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-    }
+#####################################
+# START Installation
 
-    # Versions ANzeige für Log
-    C:\ProgramData\chocolatey\choco.exe list -lo
-
-    # Parameter merken für Updates von Paketen
-    choco feature enable -n=useRememberedArgumentsForUpgrades
-    
-    exit 0
-}catch{
-    exit 1618
+if(!(test-path "C:\ProgramData\chocolatey\choco.exe")){
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 }
 
+C:\ProgramData\chocolatey\choco.exe list
 
+C:\ProgramData\chocolatey\choco.exe feature enable -n=useRememberedArgumentsForUpgrades
+
+## Detection Key
+$Path = $($AppInfo.detection.keyPath).replace("HKEY_LOCAL_MACHINE","HKLM:")
+$Key = $($AppInfo.detection.Registry.Name)
+$KeyFormat = $($AppInfo.detection.Registry.Type)
+$Value = $($AppInfo.detection.Registry.Value)
+
+if(!(Test-Path $Path)){New-Item -Path $Path -Force}
+if(!$Key){Set-Item -Path $Path -Value $Value
+}else{Set-ItemProperty -Path $Path -Name $Key -Value $Value -Type $KeyFormat}
+
+
+# END Installation
+#####################################
+
+}
+catch{
+    $_
+}
 Stop-Transcript
