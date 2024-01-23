dde0bd97975665a466aa7682fb62fa27db467454
Florian Salzmann <38555854+FlorianSLZ@users.noreply.github.com>
2024-01-23 14:27:59 +0100

    new teams backgrounds

--- Teams/Teams-Backgrounds/install.ps1
+++ Teams/Teams-Backgrounds/install.ps1
@@ -1,34 +1,53 @@
-﻿$PackageName = "Teams-Backgrounds"
+﻿$PackageName = "Teams-new-Backgrounds"
 $Version = "1"
 
 Start-Transcript -Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\$PackageName-$env:USERNAME-install.log" -Force
-$ErrorActionPreference = "Stop"
 
 try{
     # Local Folder 
     $TeamsBG_Folder = "$env:LOCALAPPDATA\Packages\MSTeams_8wekyb3d8bbwe\LocalCache\Microsoft\MSTeams\Backgrounds\Uploads"
+
+    # Reference IDs
+    $TeamsBG_ref = "$TeamsBG_Folder\TeamsBackgrounds.csv"
     
-    # Sicherstelen, das der Ordner vorhanden ist
-    New-Item -ItemType directory -Path $TeamsBG_Folder -Force
+    # Create folder if not exists
+    if(!$(Test-Path $TeamsBG_Folder)){New-Item -ItemType directory -Path $TeamsBG_Folder -Force}
+
+    if(Test-Path $TeamsBG_ref){
+        # Clean up old files
+        $TeamsBG_old = Import-Csv -Path $TeamsBG_ref
+        foreach($TeamsBG in $TeamsBG_old.Name){
+            Write-Host "Removing $TeamsBG ..."
+            Remove-Item -Path "$TeamsBG_Folder\$TeamsBG*" -Force
+        }
+    }else{
+        Write-Host "No old files found"
+    }
     
-    # Hintergründe kopieren
+    # Copy Backgrounds and rename with GUID
     $vBackgrounds = Get-ChildItem -Path '.\bg' 
+    $vBackgrounds_GUID = @()
     foreach($vBackground in $vBackgrounds){
-        $vBackground_guid = [guid]::NewGuid().Guid
-        Copy-Item -path $vBackground.FullName -Destination "$TeamsBG_Folder\$vBackground_guid$($vBackground.Extension)" -Force -WhatIf
+        $vBackground_guid = [guid]::NewGuid().GUID
+        Write-Host "Saving $($vBackground.Name) as $vBackground_guid ..."
+        Copy-Item -Path $vBackground.FullName -Destination "$TeamsBG_Folder\$vBackground_guid$($vBackground.Extension)" -Force
+        Copy-Item -Path $vBackground.FullName -Destination $("$TeamsBG_Folder\$vBackground_guid" + "_thumb" + "$($vBackground.Extension)") -Force
+
+        $vBackgrounds_GUID += $vBackground_guid
 
     }
 
+    $vBackgrounds_GUID | Out-File $TeamsBG_ref -Force
+    $obj_list = $vBackgrounds_GUID | Select-Object @{Name='Name';Expression={$_}}
+    $obj_list | Export-Csv $TeamsBG_ref -NoTypeInformation 
+
     # Validation Key
-    New-Item "HKCU:\SOFTWARE\scloud\Packages\$PackageName" -Force | New-ItemProperty -Name Version -Value $Version -Force
+    New-Item -Path "$TeamsBG_Folder\$PackageName" -ItemType File -Value $Version -Force
 
 
     
 }catch{
-    Write-Host "_____________________________________________________________________"
-    Write-Host "ERROR"
-    Write-Host "$_"
-    Write-Host "_____________________________________________________________________"
+    Write-Error "$_"
 }
 
 Stop-Transcript
--- Teams/Teams-Backgrounds/uninstall.ps1
+++ Teams/Teams-Backgrounds/uninstall.ps1
@@ -1,14 +1,33 @@
-﻿$PackageName = "Teams-Backgrounds"
+﻿$PackageName = "Teams-new-Backgrounds"
 
-Start-Transcript -Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\$PackageName-$env:USERNAME-uninstall.log" -Force
+Start-Transcript -Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\$PackageName-$env:USERNAME-install.log" -Force
 
-$TeamsBG_Folder = "$env:APPDATA\Microsoft\Teams\Backgrounds\Uploads"
-$TeamsBG_Files = Get-ChildItem -Path '.\bg' -Name
+try{
+    # Local Folder 
+    $TeamsBG_Folder = "$env:LOCALAPPDATA\Packages\MSTeams_8wekyb3d8bbwe\LocalCache\Microsoft\MSTeams\Backgrounds\Uploads"
 
-# delete deployed wallpapers
-Get-ChildItem $TeamsBG_Folder | Where-Object{$_.Name -in $TeamsBG_Files} | Remove-Item
+    # Reference IDs
+    $TeamsBG_ref = "$TeamsBG_Folder\TeamsBackgrounds.csv"
 
-# delete detection
-Remove-Item "HKCU:\SOFTWARE\scloud\Packages\$PackageName"
+    if(Test-Path $TeamsBG_ref){
+        # Clean up old files
+        $TeamsBG_old = Import-Csv -Path $TeamsBG_ref
+        foreach($TeamsBG in $TeamsBG_old.Name){
+            Write-Host "Removing $TeamsBG ..."
+            Remove-Item -Path "$TeamsBG_Folder\$TeamsBG*" -Force
+        }
+    }else{
+        Write-Host "No old files found"
+    }
+    
+
+    # Validation Key
+    Remove-Item -Path "$TeamsBG_Folder\$PackageName" -Force
+
+
+    
+}catch{
+    Write-Error "$_"
+}
 
 Stop-Transcript
