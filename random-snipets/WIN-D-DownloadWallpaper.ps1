1c9a19c39b29e31b0366da52a6c569d8c27e8acd
Florian Salzmann <38555854+FlorianSLZ@users.noreply.github.com>
2024-02-08 08:36:09 +0100

    Update WIN-D-DownloadWallpaper.ps1

--- random-snipets/WIN-D-DownloadWallpaper.ps1
+++ random-snipets/WIN-D-DownloadWallpaper.ps1
@@ -1,10 +1,10 @@
 $PackageName = "WIN-D-DownloadWallpaper"
 Start-Transcript -Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\$PackageName-script.log" -Force
 
-$Wallpaper_online = "https://github.com/FlorianSLZ/scloud/blob/main/img/scloud-wallpaper.jpg"
+$Wallpaper_online = "https://raw.githubusercontent.com/FlorianSLZ/scloud/main/img/scloud-wallpaper.jpg"
 $Wallpaper_local = "C:\Windows\Wallpaper.jpg"
 
 $wc = New-Object System.Net.WebClient
-$wc.DownloadFile($Wallpaper_online, "$Wallpaper_local")
+$wc.DownloadFile($Wallpaper_online, $Wallpaper_local)
 
 Stop-Transcript
