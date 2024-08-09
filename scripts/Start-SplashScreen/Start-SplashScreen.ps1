6103d20d7b361323fb18cbd16def7d342f4eb6bd
Florian Salzmann <38555854+FlorianSLZ@users.noreply.github.com>
2024-08-09 14:07:23 +0200

    Create Start-SplashScreen.ps1

---
+++ scripts/Start-SplashScreen/Start-SplashScreen.ps1
@@ -0,0 +1,41 @@
+
+<#PSScriptInfo
+
+.VERSION 1.0
+
+.GUID 971a9a83-2988-425d-bc7b-f9a741307ca5
+
+.AUTHOR Florian Salzmann | @FlorianSLZ | https://scloud.work
+
+.COMPANYNAME scloud.work
+
+.COPYRIGHT 2024 Florian Salzmann. GPL-3.0 license.
+
+.TAGS Tag1 Tag2 Tag3
+
+.LICENSEURI https://github.com/FlorianSLZ/scloud/blob/main/LICENSE
+
+.PROJECTURI https://contoso.com/
+
+.ICONURI https://contoso.com/Icon
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+
+
+#>
+
+<# 
+
+.DESCRIPTION 
+ Start-SplashScreen is a PowerShell script designed to execute a series of scripts with a user-friendly graphical interface. It provides a visual representation of the script execution process, including progress updates and a status indicator. The script also offers the ability to open the running PowerShell terminal window for troubleshooting or additional actions. 
+
+#> 
+Param()
+
+
