052fa08abf35c7b9ed1c2d82733757e4d11c7f44
Florian Salzmann <38555854+FlorianSLZ@users.noreply.github.com>
2024-08-13 09:42:17 +0200

    Create Autopilot-Registration_AppReg.ps1

---
+++ Windows Autopilot/Autopilot-Registration_AppReg.ps1
@@ -0,0 +1,27 @@
+<#
+.DESCRIPTION
+    Reads and upload the Autopilot information from a device to Intune. 
+.NOTES
+    Version:        1
+    Information:    https://scloud.work/autopilot-registration-app/
+    Author:         Florian Salzmann
+
+    Changes: 
+        1.0 initial version
+  
+#>
+
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Force
+Write-Host "(i) We will now install the some required components`n" -ForegroundColor Cyan
+Install-PackageProvider -Name NuGet -Force | Out-Null
+Install-Script -Name Get-WindowsAutoPilotInfo -Force | Out-Null
+Install-Module -Name WindowsAutopilotIntune -Force | Out-Null
+
+$AutopilotParams = @{
+    Online = $true
+    TenantId = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+    AppId = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+    AppSecret = "xxxx~xxxxxxxxxxxxxx~xxxxxxxxxxxxxxxxxxxxx"
+}
+
+Get-WindowsAutoPilotInfo @AutopilotParams
