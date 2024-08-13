b48571978da108619082df32b24e35125b81b6fc
Florian Salzmann <38555854+FlorianSLZ@users.noreply.github.com>
2024-08-13 10:11:21 +0200

    Update Autopilot-Registration_viaAppReg.ps1

--- Windows Autopilot/Autopilot-Registration_viaAppReg.ps1
+++ Windows Autopilot/Autopilot-Registration_viaAppReg.ps1
@@ -4,7 +4,7 @@
 .NOTES
     Version:        1
     Information:    https://scloud.work/autopilot-registration-app/
-    Author:         Florian Salzmann
+    Author:         Florian Salzmann | @FlorianSLZ | https://scloud.work
 
     Changes: 
         1.0 initial version
