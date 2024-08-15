f4079f3a6898b51313edfb445353323b2f6a6a6d
Florian Salzmann <38555854+FlorianSLZ@users.noreply.github.com>
2024-08-15 08:08:57 +0200

    v1.5
    Optimized online script handling

--- scripts/Start-SplashScreen/Start-SplashScreen.ps1
+++ scripts/Start-SplashScreen/Start-SplashScreen.ps1
@@ -1,6 +1,6 @@
 <#PSScriptInfo
 
-.VERSION 1.2
+.VERSION 1.5
 .GUID b00d1997-e5da-4af1-86f0-92120cfab2f3
 .AUTHOR Florian Salzmann
 .COMPANYNAME scloud.work
@@ -16,6 +16,9 @@
     2024-08-09, 1.0:    Original published version.
     2024-08-13, 1.1:    New icon, always in front. 
     2024-08-14, 1.2:    Removed OSDCloud dependency.
+    2024-08-14, 1.3:    -UseBasicParsing added to Invoke-WebRequest.
+    2024-08-14, 1.4:    Removed "Topmost" to allow better troubleshooting.
+    2024-08-15, 1.5:    Optimized online script handling.
 
 #> 
 
@@ -41,15 +44,15 @@ param (
     [string]$MessageStatus = "...",
 
     [parameter(Mandatory = $false, HelpMessage = "Finishing message befor Splash Screen closes")]
-    [string]$MessageFinished = "All processes finished. This windows will automaticly close in", 
+    [string]$MessageFinished = "All processes finished. This window closes automatically. ", 
 
     [parameter(Mandatory = $false, HelpMessage = "Time until Splash Screen closes after finishing")]
     [int]$ClosingTimer = 5,
 
-    [parameter(Mandatory = $false, HelpMessage = "Finishing message befor Splash Screen closes")]
-    [string]$ColorBackground = "#CCf4f4f4", 
+    [parameter(Mandatory = $false, HelpMessage = "Background color of the Splash Screen. Eg. #CCf4f4f4 (CC = 80% transparent) or #f4f4f4")]
+    [string]$ColorBackground = "#f4f4f4", 
 
-    [parameter(Mandatory = $false, HelpMessage = "Finishing message befor Splash Screen closes")]
+    [parameter(Mandatory = $false, HelpMessage = "Text color of the Splash Screen. Eg. #161616")]
     [string]$ColorText = "#161616"
 
 )
@@ -62,27 +65,30 @@ Add-Type -AssemblyName PresentationFramework
   xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
   Title="$MessageHeader"
   WindowStartupLocation="CenterScreen"
-  WindowStyle="None" 
-  ShowInTaskbar="False" 
+  WindowStyle="None"
   AllowsTransparency="True"
-  Topmost="True"
+  WindowState="Maximized"
+  ShowInTaskbar="False" 
   Background="$ColorBackground"
   Foreground="$ColorText"
-  WindowState="Maximized">
+  >
   <Grid>
     <Grid.RowDefinitions>
       <RowDefinition Height="*"/>
-      <RowDefinition Height="50"/>
+      <RowDefinition Height="75"/>
     </Grid.RowDefinitions>
     <StackPanel VerticalAlignment="Center" HorizontalAlignment="Center">
       <TextBlock Name="TextMessageHeader" Text="$MessageHeader" FontSize="32" FontWeight="Bold" TextAlignment="Center" />
-      <TextBlock Name="TextMessageBody" Text="$MessageText" FontSize="16" TextWrapping="Wrap" TextAlignment="Center" FontStyle="Italic" Margin="0,10,0,10" />
+      <TextBlock Name="TextMessageBody" Text="$MessageText" FontSize="16" TextWrapping="Wrap" TextAlignment="Center" FontStyle="Italic" Margin="0,20,0,20" />
       <TextBlock Name="TextMessageStatus" Text="$MessageStatus" FontSize="18" FontWeight="Bold" TextAlignment="Center"/>
     </StackPanel>
     <Button Grid.Row="1" Name="ShowTerminal" Content="" HorizontalAlignment="Stretch" Background="Transparent" BorderThickness="0" />
   </Grid>
 </Window>
 "@
+<#
+
+#>
 
 # Load XAML
 $reader = (New-Object System.Xml.XmlNodeReader $xaml)
@@ -111,11 +117,16 @@ public static extern IntPtr GetConsoleWindow();
 
 [DllImport("user32.dll")]
 public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
+
+[DllImport("user32.dll")]
+[return: MarshalAs(UnmanagedType.Bool)]
+public static extern bool SetForegroundWindow(IntPtr hWnd);
 '
 
 function Show-Console {
     $consolePtr = [Console.Window]::GetConsoleWindow()
     [Console.Window]::ShowWindow($consolePtr, 5)
+    [Console.Window]::SetForegroundWindow($consolePtr) # Bring the window to the front
 }
 
 function Hide-Console {
@@ -125,12 +136,11 @@ function Hide-Console {
 
 
 
+
 # Show the window
-Hide-Console
+#Hide-Console
 $Window.Show() | Out-Null
 
-# Run the Dispatcher
-#[System.Windows.Threading.Dispatcher]::Run()
 
 $counter = 0 
 $total = $Processes.Count
@@ -146,9 +156,9 @@ foreach ($script in $Processes) {
     # Check if the value is a URL (starts with "http") 
     if ($script.Script -match "^https?://") {
         Write-Output "($counter/$total) - Running online script: $($script.Script)"
-        $ScriptFile = Invoke-WebRequest $($script.Script)
+        $ScriptFile = Invoke-WebRequest $($script.Script) -UseBasicParsing
         $ScriptBlock = [Scriptblock]::Create($ScriptFile.Content) 
-        Invoke-Command -ScriptBlock $ScriptBlock -ArgumentList ($args + @('someargument'))
+        Invoke-Command -ScriptBlock $ScriptBlock
     } else {
       # Directly run the command (assuming it's a string)
       Write-Output "($counter/$total)- Running PowerShell command: $($script.Script)"
@@ -173,3 +183,5 @@ for ($i = $ClosingTimer; $i -gt 0; $i--) {
 
 # Close the window after the countdown
 $Window.Close()
+
+Show-Console
