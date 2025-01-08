6c5cea6f318923427cf6cc4231d5710062064206
Florian Salzmann <38555854+FlorianSLZ@users.noreply.github.com>
2025-01-08 11:19:22 +0100

    New Teams Installation bootstrap update

---
+++ Program - win32/Microsoft Teams (new) - Classic uninstall/install.ps1
@@ -0,0 +1,86 @@
+﻿$PackageName = "MicrosoftTeamsNEW"
+$LogPath = "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\$PackageName-install.log"
+
+# Start transcript logging
+Start-Transcript -Path $LogPath -Force
+
+###########################################################
+# Teams Classic cleanup
+###########################################################
+
+# Function to uninstall Teams Classic
+function Uninstall-TeamsClassic($TeamsPath) {
+    try {
+        $process = Start-Process -FilePath "$TeamsPath\Update.exe" -ArgumentList "--uninstall /s" -PassThru -Wait -ErrorAction STOP
+
+        if ($process.ExitCode -ne 0) {
+            Write-Error "Uninstallation failed with exit code $($process.ExitCode)."
+        }
+    }
+    catch {
+        Write-Error $_.Exception.Message
+    }
+}
+
+# Remove Teams Machine-Wide Installer
+Write-Host "Removing Teams Machine-wide Installer"
+
+#Windows Uninstaller Registry Path
+$registryPath = "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
+
+# Get all subkeys and match the subkey that contains "Teams Machine-Wide Installer" DisplayName.
+$MachineWide = Get-ItemProperty -Path $registryPath | Where-Object -Property DisplayName -eq "Teams Machine-Wide Installer"
+
+if ($MachineWide) {
+    Start-Process -FilePath "msiexec.exe" -ArgumentList "/x ""$($MachineWide.PSChildName)"" /qn" -NoNewWindow -Wait
+}
+else {
+    Write-Host "Teams Machine-Wide Installer not found"
+}
+
+# Get all Users
+$AllUsers = Get-ChildItem -Path "$($ENV:SystemDrive)\Users"
+
+# Process all Users
+foreach ($User in $AllUsers) {
+    Write-Host "Processing user: $($User.Name)"
+
+    # Locate installation folder
+    $localAppData = "$($ENV:SystemDrive)\Users\$($User.Name)\AppData\Local\Microsoft\Teams"
+    $programData = "$($env:ProgramData)\$($User.Name)\Microsoft\Teams"
+
+    if (Test-Path "$localAppData\Current\Teams.exe") {
+        Write-Host "  Uninstall Teams for user $($User.Name)"
+        Uninstall-TeamsClassic -TeamsPath $localAppData
+    }
+    elseif (Test-Path "$programData\Current\Teams.exe") {
+        Write-Host "  Uninstall Teams for user $($User.Name)"
+        Uninstall-TeamsClassic -TeamsPath $programData
+    }
+    else {
+        Write-Host "  Teams installation not found for user $($User.Name)"
+    }
+}
+
+# Remove old Teams folders and icons
+$TeamsFolder_old = "$($ENV:SystemDrive)\Users\*\AppData\Local\Microsoft\Teams"
+$TeamsIcon_old = "$($ENV:SystemDrive)\Users\*\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Microsoft Teams*.lnk"
+Get-Item $TeamsFolder_old | Remove-Item -Force -Recurse
+Get-Item $TeamsIcon_old | Remove-Item -Force -Recurse
+
+###########################################################
+# New Teams installation
+###########################################################
+
+Write-Host "Installing new Teams"
+# Get the current directory
+$currentDirectory = Split-Path -Parent $PSCommandPath
+
+# Build the path to the exe file
+$exePath = Join-Path -Path $currentDirectory -ChildPath "teamsbootstrapper.exe"
+
+# Start the exe process
+Start-Process -FilePath $exePath -ArgumentList "-p" -NoNewWindow -Wait
+
+# Stop transcript logging
+Stop-Transcript
