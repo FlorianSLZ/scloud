41fb288c20a64ed2f0e804242d1a5ce1a671012d
Florian Salzmann <38555854+FlorianSLZ@users.noreply.github.com>
2025-03-14 08:56:49 +0100

    v1.7
    Added optional LogoURL parameter

--- scripts/Start-SplashScreen/Start-SplashScreen.ps1
+++ scripts/Start-SplashScreen/Start-SplashScreen.ps1
@@ -1,6 +1,6 @@
 <#PSScriptInfo
 
-.VERSION 1.6
+.VERSION 1.7
 .GUID b00d1997-e5da-4af1-86f0-92120cfab2f3
 .AUTHOR Florian Salzmann
 .COMPANYNAME scloud.work
@@ -20,6 +20,7 @@
     2024-08-14, 1.4:    Removed "Topmost" to allow better troubleshooting.
     2024-08-15, 1.5:    Optimized online script handling.
     2024-08-15, 1.6:    Added support for http and ftp.
+    2025-03-14, 1.7:    Added optional LogoURL parameter.
 
 #> 
 
@@ -28,36 +29,50 @@
 .DESCRIPTION 
 Start-SplashScreen is a PowerShell script designed to execute a series of scripts with a user-friendly graphical interface. It provides a visual representation of the script execution process, including progress updates and a status indicator. The script also offers the ability to open the running PowerShell terminal window for troubleshooting or additional actions.
 
+.EXAMPLE
+$Scripts2run = @(
+  @{
+    Name = "Dummy Wait for 15s"
+    Script = "Start-Sleep -s 15"
+  },
+  @{
+    Name = "Windows Quality Updates"
+    Script = "https://raw.githubusercontent.com/FlorianSLZ/OSDCloud-Stuff/main/OOBE/Windows-Updates_Quality.ps1"
+  }
+)
+
+Start-SplashScreen.ps1 -Processes $Scripts2run -LogoURL "https://scloud.work/wp-content/uploads/2023/08/terminal-logo-scloud.png"
+
 #> 
 
-
 param (
-    [parameter(Mandatory = $true, HelpMessage = "Specify the processes by name and powershell-command or https-link. ")]
+    [parameter(Mandatory = $true, HelpMessage = "Specify the processes by name and PowerShell-command or https-link.")]
     [array]$Processes,
     
     [parameter(Mandatory = $false, HelpMessage = "Main message on the Splash Screen.")]
-    [string]$MessageHeader = "Windows Preperation",
+    [string]$MessageHeader = "Windows Preparation",
     
-    [parameter(Mandatory = $false, HelpMessage = "Initla message where the script names will show on the Splash Screen (should appear less than a second).")]
+    [parameter(Mandatory = $false, HelpMessage = "Initial message where the script names will show on the Splash Screen.")]
     [string]$MessageText = "Initiate Installation",
     
-    [parameter(Mandatory = $false, HelpMessage = "Initla status idendicator on the Splash Screen (should appear less than a second).")]
+    [parameter(Mandatory = $false, HelpMessage = "Initial status indicator on the Splash Screen.")]
     [string]$MessageStatus = "...",
 
-    [parameter(Mandatory = $false, HelpMessage = "Finishing message befor Splash Screen closes")]
-    [string]$MessageFinished = "All processes finished. This window closes automatically. ", 
+    [parameter(Mandatory = $false, HelpMessage = "Finishing message before Splash Screen closes.")]
+    [string]$MessageFinished = "All processes finished. This window closes automatically.", 
 
-    [parameter(Mandatory = $false, HelpMessage = "Time until Splash Screen closes after finishing")]
+    [parameter(Mandatory = $false, HelpMessage = "Time until Splash Screen closes after finishing.")]
     [int]$ClosingTimer = 5,
 
     [parameter(Mandatory = $false, HelpMessage = "Background color of the Splash Screen. Eg. #CCf4f4f4 (CC = 80% transparent) or #f4f4f4")]
     [string]$ColorBackground = "#f4f4f4", 
 
     [parameter(Mandatory = $false, HelpMessage = "Text color of the Splash Screen. Eg. #161616")]
-    [string]$ColorText = "#161616"
+    [string]$ColorText = "#161616",
 
+    [parameter(Mandatory = $false, HelpMessage = "Optional Logo URL.")]
+    [string]$LogoURL = ""
 )
-
 
 Add-Type -AssemblyName PresentationFramework
 [XML]$xaml = @"
@@ -79,6 +94,7 @@ Add-Type -AssemblyName PresentationFramework
       <RowDefinition Height="75"/>
     </Grid.RowDefinitions>
     <StackPanel VerticalAlignment="Center" HorizontalAlignment="Center">
+      <Image Name="LogoImage" Width="200" Height="200" Margin="0,0,0,20" Visibility="Hidden"/>
       <TextBlock Name="TextMessageHeader" Text="$MessageHeader" FontSize="32" FontWeight="Bold" TextAlignment="Center" />
       <TextBlock Name="TextMessageBody" Text="$MessageText" FontSize="16" TextWrapping="Wrap" TextAlignment="Center" FontStyle="Italic" Margin="0,20,0,20" />
       <TextBlock Name="TextMessageStatus" Text="$MessageStatus" FontSize="18" FontWeight="Bold" TextAlignment="Center"/>
@@ -87,31 +103,35 @@ Add-Type -AssemblyName PresentationFramework
   </Grid>
 </Window>
 "@
-<#
-
-#>
 
 # Load XAML
 $reader = (New-Object System.Xml.XmlNodeReader $xaml)
 $Window = [Windows.Markup.XamlReader]::Load($reader)
 
-# Create a DispatcherTimer for the "Later" button action
-$timer = New-Object System.Windows.Threading.DispatcherTimer
-$timer.Interval = [TimeSpan]::FromMinutes(15)
-$timer.Add_Tick({
-    $timer.Stop()
-    $Window.Show()
-}) 
+# Load the logo if provided
+$LogoImage = $Window.FindName("LogoImage")
+if (-not [string]::IsNullOrWhiteSpace($LogoURL)) {
+    try {
+        $bitmap = New-Object System.Windows.Media.Imaging.BitmapImage
+        $bitmap.BeginInit()
+        $bitmap.UriSource = New-Object System.Uri($LogoURL)
+        $bitmap.EndInit()
+        $LogoImage.Source = $bitmap
+        $LogoImage.Visibility = "Visible"
+    }
+    catch {
+        Write-Warning "Failed to load logo from $LogoURL"
+    }
+}
 
-
+# UI Elements
 $messageScreenText = $Window.FindName("TextMessageBody")
 $messageScreenStatus = $Window.FindName("TextMessageStatus")
 
 $ShowTerminalButton = $Window.FindName("ShowTerminal")
 $ShowTerminalButton.Add_Click({ Show-Console })
 
-
-# Credits to - http://powershell.cz/2013/04/04/hide-and-show-console-window-from-gui/
+# Show/Hide Console Functions
 Add-Type -Name Window -Namespace Console -MemberDefinition '
 [DllImport("Kernel32.dll")]
 public static extern IntPtr GetConsoleWindow();
@@ -127,7 +147,7 @@ public static extern bool SetForegroundWindow(IntPtr hWnd);
 function Show-Console {
     $consolePtr = [Console.Window]::GetConsoleWindow()
     [Console.Window]::ShowWindow($consolePtr, 5)
-    [Console.Window]::SetForegroundWindow($consolePtr) # Bring the window to the front
+    [Console.Window]::SetForegroundWindow($consolePtr)
 }
 
 function Hide-Console {
@@ -135,14 +155,11 @@ function Hide-Console {
     [Console.Window]::ShowWindow($consolePtr, 0)
 }
 
-
-
-
 # Show the window
-#Hide-Console
+# Hide-Console
 $Window.Show() | Out-Null
 
-
+# Process scripts
 $counter = 0 
 $total = $Processes.Count
 foreach ($script in $Processes) {
@@ -150,44 +167,26 @@ foreach ($script in $Processes) {
     $messageScreenText.Text = "$($script.Name)"
     $messageScreenStatus.Text = "($counter/$total)"
 
-    
     $Window.Dispatcher.Invoke([System.Windows.Threading.DispatcherPriority]::Background, [System.Action]{})
 
-
-    # Check if the value is a URL (starts with "http") 
     if ($script.Script -match "^https?://" -or $script.Script -match "^http://" -or $script.Script -match "^ftp://") {
         Write-Output "($counter/$total) - Running online script: $($script.Script)"
-
-        # Download the script and run it
-        if (-NOT (Test-WebConnection "$($script.Script)")) {Write-Warning "Script not available: $($script.Script)"; continue}
         $WebClient = New-Object System.Net.WebClient
         $WebPSCommand = $WebClient.DownloadString("$($script.Script)")
         Invoke-Expression -Command $WebPSCommand
         $WebClient.Dispose()
-
     } else {
-      # Directly run the command (assuming it's a string)
-      Write-Output "($counter/$total)- Running PowerShell command: $($script.Script)"
-      Invoke-Expression $($script.Script)
+        Write-Output "($counter/$total) - Running PowerShell command: $($script.Script)"
+        Invoke-Expression $($script.Script)
     }
 }
 
-
-# Update the UI with the final message and countdown timer
 $messageScreenText.Text = $MessageFinished
-
-# Countdown loop
 for ($i = $ClosingTimer; $i -gt 0; $i--) {
     $messageScreenStatus.Text = "$i Seconds"
-
-    # Update the UI
     $Window.Dispatcher.Invoke([System.Windows.Threading.DispatcherPriority]::Background, [System.Action]{})
-    
-    # Wait for 1 second
     Start-Sleep -Seconds 1
 }
 
-# Close the window after the countdown
 $Window.Close()
-
 Show-Console
